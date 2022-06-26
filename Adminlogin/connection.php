bb50a95b57125c2119a6d6cfa5e04695edd59574
Sne21ha <snehakoliyat@lbscek.ac.in>
2022-06-26 22:54:18 +0530

    4th commit

---
+++ Adminlogin/connection.php
@@ -0,0 +1,15 @@
+
+<?php  
+
+$sname = "localhost";
+$uname = "root";
+$password = "";
+
+$db_name = "Invoice_system";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection Failed!";
+	exit();
+}
