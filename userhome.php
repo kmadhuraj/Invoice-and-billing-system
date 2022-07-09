f16089dd778609fa0411f70e1119175a16c1114f
MohdShibily <shibilymohd73@gmail.com>
2022-07-09 20:55:24 +0530

    userhome and validate connection file

---
+++ userhome.php
@@ -0,0 +1,33 @@
+<?php
+
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-BILLING AND INVOICE SYSTEM</title>
+  </head>
+  <body>
+
+  <div class="container">
+
+<div class="top-buttons btn-group">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Items <span class="caret"></span></button>
+      <ul class="dropdown-menu" role="menu">	
+         
+      
+         <li><a href="invoice-list.php">View Product</a></li> 
+
+
+
+       
+      
+</div>
+
+
+
+
+  </body>
+</html>
