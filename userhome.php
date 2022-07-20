a50679a9186edcafe81e6a11fba48c4a9418108a
kmadhuraj <madhurajmadu693@gmail.com>
2022-07-20 21:35:13 +0530

    view button alignment is changed

--- userhome.php
+++ userhome.php
@@ -5,21 +5,57 @@
 <!doctype html>
 <html lang="en">
   <head>
+
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-BILLING AND INVOICE SYSTEM</title>
+ 
   </head>
   <body>
 
   <div class="container">
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-<div class="top-buttons btn-group">
-    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Items <span class="caret"></span></button>
-      <ul class="dropdown-menu" role="menu">	
-         
-      
-         <li><a href="invoice-list.php">View Product</a></li> 
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Back</a>
+        </li>
+<!-- 
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li> -->
 
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+  <div class="container">
+ 
+  <div class="d-grid gap-2 col-2 mx-auto">
+    <a href="#">
+  <button class="btn btn-primary" type="button"> View Products</button>
+  </a>
+
+</div>
 
 
        
