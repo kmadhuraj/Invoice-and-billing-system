5f304e265467722a56c53df75d8d7d72873ba0d8
kmadhuraj <madhurajmadu693@gmail.com>
2022-08-30 13:40:18 +0530

    last update2

---
+++ deleteuser.php
@@ -0,0 +1,67 @@
+<?php
+include "connection.php";
+
+
+$sl_no=$_GET['slno'];
+$sql="DELETE FROM `userlogin` WHERE sl_no ='$sl_no'";
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deletion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+</head>
+<body>
+  
+<div class="container">
+			
+            <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                
+    
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="user-manage.php">Back</a>
+                    </li>
+    
+                  
+    
+                </ul>
+                </div>
+            </div>
+            
+            </nav>
+            
+
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        
+</body>
+</html>
+<?php
+if($result)
+{
+    echo' <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Succesfully Deleted</strong> From the Database
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div> ';
+}
+else{
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+				<strong>Deletion Failed!</strong> 
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div> ';
+}
+
+?>
